afaaf11a8a4e11a2e5a3a6aeffce414dc3bf3347
Robert Santos <robertdahora@gmail.com>
2023-09-09 10:49:36 -0300

    exercicio typescript

---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+const multiplica = (numeroA: number, numeroB: number):number => {
+    return numeroA * numeroB;
+};
+
+const saudacao = (nome: string): string => {
+    return `Olá, ${nome}`;
+};
+
+console.log(multiplica(5, 6));
+console.log(saudacao('Robert'));
